4d12bd569c3042cc35b4b8803ea92969fde87a08
Bathul Fathima.S <sbathulshaik73@gmail.com>
2026-08-01 21:06:36 +0530

    Create B-Compression.java

---
+++ B-Compression.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class solution{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        Set<Integer>  b = new TreeSet<>();
+        for(int i=1;i<=N;i++){
+            s.add(sc.nextInt());
+        }
+        System.out.println(b.size());
+        for(int k : b){
+            System.out.print(k+" ");
+        }
+    }
+}
